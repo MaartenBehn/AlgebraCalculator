f68ba07a82e98889ea7d21acef5b7608a0a3035f
Stroby241 <maarten.behn@gmail.com>
2021-03-09 22:34:18 +0100

    small dir fix

--- app/fyne/build/update-bundle.cmd
+++ app/fyne/build/update-bundle.cmd
@@ -1,3 +1,4 @@
-fyne bundle D:\dev\Go\src\AlgebraCalculator\V3\rules\simpRulesExpand.txt > bundled.go
-fyne bundle -append D:\dev\Go\src\AlgebraCalculator\V3\rules\simpRulesSumUp.txt >> bundled.go
-move "bundled.go" "../AlgebraCalculator/"
+cd D:\dev\Go\src\AlgebraCalculator\ruleFiles
+fyne bundle simpRulesExpand.txt > bundled.go
+fyne bundle -append simpRulesSumUp.txt >> bundled.go
+move "bundled.go" "D:\dev\Go\src\AlgebraCalculator\app\fyne\AlgebraCalculator"
